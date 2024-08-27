ee4a7a658eec8b0339f24538ae2042bbb8218d36
sorooshAfhami <122637590+sorooshAfhami@users.noreply.github.com>
2024-08-27 11:42:19 +0330

    Update Program.cs

--- ConsoleFloxCartBot/Program.cs
+++ ConsoleFloxCartBot/Program.cs
@@ -18,6 +18,8 @@ List<string> list = new List<string>();
 using var cts = new CancellationTokenSource();
 var bot = new TelegramBotClient("7218401198:AAGRcEI1y09uhV-CtwIsoeh4r9JLKLuVm5k", cancellationToken: cts.Token);
 var me = await bot.GetMeAsync();
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate(before on message)");
+
 bot.OnMessage += OnMessage;
 
 Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
